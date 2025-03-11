d01909474d9ac9530585d6aa9de1b0534fcc94ec
Yonas Solomon <yonas.solomonf@aastustudent.edu.et>
2025-03-11 10:13:42 -0900

    Create app.jsx

---
+++ task5/app.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import Homee from "./taskk4/Homee";
+import About from "./taskk4/About";
+import Contact from "./taskk4/Contact";
+import Notfound from "./taskk4/404page";
+import Themecontext from "./task5/ThemeCOntext";
+import Counter from "./MOSH/counter";
+import Movie from "./MOSH/movie";
+function App(){
+  return(
+    <div>
+      <Themecontext/>
+    </div>
+  )
+}
+export default App
